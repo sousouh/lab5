1ed421a82a3f43f3e541e69462e357089da6b9f2
Soudeh Jahanshahi <sjahanshahi@pc01.pool.tp1.uni-duesseldorf.de>
2015-11-24 16:02:40 +0100

    changed it

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -10,7 +10,46 @@ struct colloid{
 };
 
 void init(colloid* const c, const int N);
+
 void print(const colloid* const c, const int N, const string fname);
+
+void condition(int* rx, int* ry, const int N){
+ 
+  for(int i=0; i<N; i++){
+    rx[i]=rand()%3-1;
+  }
+
+  for(int i=0; i<N; i++){
+    ry[i]=rand()%3-1;
+  }
+  }
+
+void pusher(colloid* const c, const int N, int* rx, int* ry, const double step){
+ for(int i=0; i<N; i++){
+  c[i].x += rx[i]*step;
+  c[i].y += ry[i]*step;
+ }
+}
+
+void statistics(const colloid* const c, double& meanx, double& meany,double& variance, const int N){
+ meanx=0;
+ meany=0;
+ variance=0;
+  for(int i=0; i<N; i++){
+   meanx+=c[i].x;
+   meany+=c[i].y;
+  }
+ meanx/=N;
+ meany/=N;
+for(int i=0; i<N; i++){
+  
+  variance+=(c[i].x-meanx)*(c[i].x-meanx)+(c[i].y-meany)*(c[i].y-meany);
+  
+}
+variance/=N;
+   
+}
+
 
 int main(void){
   
@@ -45,8 +84,11 @@ int main(void){
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
 	    // call to function which randomly sets up rx and ry
+	  condition(rx,ry,N);
 	    // call to function which pushes all colloids according to rx and ry
+	  pusher(c,N,rx,ry,step);
 	    // call to function which evaluates statistics
+	  statistics(c,meanx,meany,var,N);
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
 	    stat << meany << "\t" << var << endl;
 	}
